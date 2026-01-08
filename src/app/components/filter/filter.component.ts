172f8fcbc4a2f9136b37aaec7bb6b2f2b0b71fbd
GustavoEmiliano <gustavoemiliano2014@gmail.com>
2026-01-08 09:19:53 -0300

    feat: add Output event emission from filter button to parent

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IFilterOptions } from '../../interfaces/filter-options.interface';
 
 @Component({
@@ -18,8 +18,9 @@ export class FilterComponent {
     { description: 'Inativo', value: false },
   ]
 
-  onFilter() {
-    console.log(this.filterOptions);
+@Output('onFilter') onFilterEmitt = new EventEmitter<IFilterOptions>();
 
+  onFilter() {
+    this.onFilterEmitt.emit(this.filterOptions);
   }
 }
